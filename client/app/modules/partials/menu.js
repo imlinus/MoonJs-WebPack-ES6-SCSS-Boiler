7e7c4c9d48224c7a8402f0dd811371d56c4b655c
Linus <liljalinus@gmail.com>
2017-09-28 17:58:49 +0200

    removed most of scss

--- client/app/modules/partials/menu.js
+++ client/app/modules/partials/menu.js
@@ -1,7 +1,7 @@
 import Moon from 'moonjs'
 
 export const Menu = Moon.component('menu', {
-  template: `<nav class="nav">
+  template: `<nav class="menu">
                <ul>
                  <li><router-link to="/">Home</router-link></li>
                  <li><router-link to="/contact">Contact</router-link></li>
